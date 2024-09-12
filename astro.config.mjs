06f1e28a5d50612054fb20f9f6cd9d2090d857a6
alexroel <alexroel@example.com>
2024-09-11 21:06:23 -0500

    rustcode-v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,38 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
     integrations: [
         starlight({
-            
-            title: 'RustCode',
+            title: 'RustCode', 
+			defaultLocale: 'es',
+			locales:{
+				root: {
+					label: 'Español',
+					lang: 'es',
+				},
+			},
             social: {
                 github: 'https://roelcode.com/',
                 facebook: 'https://roelcode.com/',
-                tiktok: 'https://roelcode.com/',
                 youtube: 'https://roelcode.com/',
             },
+			customCss: [
+				// Ruta relativa a tu archivo CSS personalizado
+				'./src/styles/custom.css',
+			],
             sidebar: [
-				{
+				/*{
 					label: 'Introducción',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Introducción', slug: 'intro/intro' },
 					],
+				},*/
+                {
+					label: 'Introducción a Rust',
+					autogenerate: { directory: '01-intro-rust' },
+				},
+				{
+					label: 'Fundamentos',
+					autogenerate: { directory: '02-fundamentos' },
 				},
 			],
         }),
