70951392861c6be0ce7d71c1377f708720994c29
Alex Roel <alexreol@git.com>
2024-09-11 19:15:17 -0500

    rustcode-v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        starlight({
+            
+            title: 'RustCode',
+            social: {
+                github: 'https://roelcode.com/',
+                facebook: 'https://roelcode.com/',
+                tiktok: 'https://roelcode.com/',
+                youtube: 'https://roelcode.com/',
+            },
+            sidebar: [
+				{
+					label: 'Introducción',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Introducción', slug: 'intro/intro' },
+					],
+				},
+			],
+        }),
+    ],
+});
